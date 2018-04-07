93bb0341ba89cbcdb7d407fce9292bc906af675c
Santhosh Kumar Prakash <santhoshpkumar@users.noreply.github.com>
2018-04-07 12:06:53 +0530

    Add files via upload

--- model.py
+++ model.py
@@ -11,47 +11,39 @@ from keras.regularizers import l2
 from keras.optimizers import Adam
 from keras.callbacks import ModelCheckpoint
 
-lines = []
-with open('./data/driving_log.csv') as csvfile:
-    reader = csv.reader(csvfile)
-    for line in reader:
-        lines.append(line)
 
-print("Loading images data....")
-images = []
-measurements = []
-current_path = './data/IMG/' 
-for line in lines:
-    steering_center  = float(line[3])
-    correction = 0.3 # this is a parameter to tune
-    steering_left = steering_center + correction
-    steering_right = steering_center - correction
-    
-    image_center = cv2.imread(current_path+line[0].split('/')[-1])
-    image_left = cv2.imread(current_path+line[1].split('/')[-1])
-    image_right = cv2.imread(current_path+line[2].split('/')[-1])
+## Helper Functions
 
-    # add images and angles to data set
-    images.extend((image_center, image_left, image_right))
-    measurements.extend((steering_center, steering_left, steering_right))
-print("Completed loading images data....")
-	  
-print("Augmenting images data....")
-augmented_images, augmented_measurements = [], []
-for image, measurement in zip(images, measurements):
-    augmented_images.append(image)
-    augmented_measurements.append(measurement)
-    augmented_images.append(cv2.flip(image,1))
-    augmented_measurements.append(measurement*-1.0)
-print("Complete augmenting images data....")
-print("Model Training and Building")
-	  
-X_train = np.array(augmented_images)
-y_train = np.array(augmented_measurements)
+def generator(samples, batch_size=32):
+    num_samples = len(samples)
+    while 1: # Loop forever so the generator never terminates
+        shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
+
+            images = []
+            angles = []
+            for batch_sample in batch_samples:
+                name = './IMG/'+batch_sample[0].split('/')[-1]
+                center_image = cv2.imread(name)
+                center_angle = float(batch_sample[3])
+                images.append(center_image)
+                angles.append(center_angle)
+
+            # trim image to only see section with road
+            X_train = np.array(images)
+            y_train = np.array(angles)
+            yield sklearn.utils.shuffle(X_train, y_train)
+
+## End Helper Functions Block
+
+
+-
 
 model = Sequential()
 
 '''
+# MODEL 7
 model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
 model.add(Cropping2D(cropping=((70,25), (0,0))))
 model.add(Convolution2D(6, 5, 5, activation='relu'))
@@ -65,6 +57,8 @@ model.add(Dense(64))
 model.add(Dense(1))
 '''
 
+# MODEL 8
+model.add(Lambda(lambda x: (x / 127.5) - 1., input_shape = (160, 320, 3)))
 model.add( Cropping2D( cropping=( (50,20), (0,0) ), input_shape=(160,320,3)))
 #Normalize the data.
 model.add( Lambda( lambda x: x/255. - 0.5 ) )
